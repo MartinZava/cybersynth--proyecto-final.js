7cbad396b1e0952b65625d06401932e04b30d63c
MartinZava <zava810@gmail.com>
2022-09-28 17:04:49 -0300

    agregue localstorage, modifique estilos

--- main.js
+++ main.js
@@ -11,9 +11,11 @@ class Sintetizador {
     }
 }
 
+
+
 // Array carrito inicializado vacio
 let carrito = []
-// Array y objetos pusheados
+// Array del stock y objetos pusheados
 let sintesStock = [];
 
 sintesStock.push(new Sintetizador("Korg", "Ms-20", 1, "Monofonico", 550, 4, "../img/ms20.jpg"))
@@ -29,25 +31,143 @@ sintesStock.push(new Sintetizador("Roland", "TR-808", 10, "Monofonico", 5000, 2,
 
 
 
+
+// DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("carrito")){
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+        renderCarrito()
+    }
+})
+
+
+
+
+// Variable para llamar por id al contenedor de las cards de cada producto
+const contenedorSintes = document.querySelector("#contenedor");
+
+
+//Funcion para "pintar" las cards de productos en el HTML
 function renderCards() {
-
-    const contenedorSintes = document.getElementById('contenedor')
-
     sintesStock.forEach((sinte) => {
-        const div = document.createElement("div")
-        div.classList.add("card my-3 mx-3 sinte-card")
+        const div = document.createElement("div");
+        div.classList.add("sinte-card");
         div.innerHTML = `
-        <img src="${sinte.img}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${sinte.marca} - ${sinte.modelo}</h5>
-            <p class="card-text">USD$${sinte.precio}</p>
-            <a href="#" class="btn btn-primary">Agregar al Carrito</a>
-        </div>`
+        <div class="card my-3 mx-3 border-info" style="width: 18rem;">
+                <img src="${sinte.img}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${sinte.marca} - ${sinte.modelo}</h5>
+                    <p class="card-text">USD$${sinte.precio}</p>
+                    <button type="button" class="btn btn-primary boton-agregar" id="agregar${sinte.id}">Agregar al Carrito</button>
+                </div>
+            </div>`;
 
+        contenedorSintes.appendChild(div);
 
-    contenedorSintes.appendChild(div)
-    })
+        // Evento que se le aplica al boton para agregar productos al carrito, llamando a la funcion addToCart
+        const botonAgregar = document.querySelector(`#agregar${sinte.id}`);
+        botonAgregar.addEventListener("click", () => {
+            addToCart(sinte.id);
+        });
+    });
 }
+
+renderCards()
+
+
+
+// Variables para llamar por id al contenedor del carrito, al contador de prod dentro del carrito y al total del carrito
+const contenedorCarrito = document.querySelector("#contenedor-carrito");
+const numCarrito = document.querySelector("#num-carrito")
+const totalCarrito = document.querySelector("#total")
+
+
+// Funcion para ir actualizando el carrito a medida que se van agregando productos
+function renderCarrito() {
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach((sinte) => {
+        const div = document.createElement("div")
+        div.classList.add("lista-carrito")
+        div.innerHTML = `
+                    <span class="border border-2"><img src="${sinte.img}" class="img-carrito rounded" alt=""></span>
+                    <p>${sinte.marca} - ${sinte.modelo}</p>
+                    <p>USD$${sinte.precio}</p>
+                    <p>Cantidad: ${sinte.cantidad}</p>
+                    <button type="button" class="btn btn-danger boton-agregar" onclick="quitarSinte(${sinte.id})"><i class="bi bi-trash"></i></button>`
+        
+        contenedorCarrito.appendChild(div)
+
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+    })
+    // Contador del carrito
+    numCarrito.innerText = carrito.lenght
+    // Acumulador del precio total, va acumulando el precio del prod por la cantidad agregada al carrito
+    totalCarrito.innerText = carrito.reduce((acc, sinte) => acc + sinte.precio * sinte.cantidad, 0)
+}
+
+
+
+
+// Funcion para añadir productos al carrito
+const addToCart = (sinteId) => {
+    // Primero pregunto si existe el producto en el carrito con el metodo .some, usando el id de cada uno
+    const existe = carrito.some (sinte => sinte.id === sinteId)
+    if(existe){
+        const sinte = carrito.map (sinte => {
+            if(sinte.id === sinteId){ // Si ese prod ya exixte, lo suma a la cantidad del mismo
+                sinte.cantidad ++
+            }
+        })
+    } else { // Si no, lo agrega al carrito
+        const instrumAgregado = sintesStock.find((sinte) => sinte.id === sinteId)
+        carrito.push(instrumAgregado)
+    }
+    renderCarrito()
+}
+
+
+
+// Funcion para quitar un producto del carrito
+const quitarSinte = (sinteId) => {
+    const sinte = carrito.find((sinte) => sinte.id === sinteId)
+    const i = carrito.indexOf(sinte)
+    carrito.splice(i, 1)
+    renderCarrito()
+}
+
+
+
+// Funcion para vaciar el carrito
+const vaciarCarrito = document.querySelector("#vaciar-carrito")
+vaciarCarrito.addEventListener("click", () =>{
+    carrito.length = 0
+    renderCarrito()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -140,19 +260,6 @@ if (continuar == "fin") {
                             2. Eliminar un producto del carrito.
                             3. Vaciar carrito.`))
 } 
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*
